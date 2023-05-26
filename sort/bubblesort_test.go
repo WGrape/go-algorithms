66315a9c84e5ffe304d43e61c0acbf88b633af60
Awail <lidaiwen66@163.com>
2023-05-26 23:50:42 +0800

    测试用例修改

--- sort/bubblesort_test.go
+++ sort/bubblesort_test.go
@@ -1,11 +1,21 @@
 package sort
 
 import (
-	"fmt"
+	"reflect"
 	"testing"
 
 )
 
 func TestPopSort(t *testing.T)  {
-	fmt.Printf("%v", BubbleSort([]int{12, 10, 25, 7, 1, 9},2))
+	arr := []int{12, 10, 25, 7, 1, 9}
+	res := BubbleSort(arr,2)
+	descArr := []int{25,12, 10, 9,7,1}
+	if !reflect.DeepEqual(res, descArr) {
+		t.Fatal("降序失败")
+	}
+	res = BubbleSort(arr,1)
+	ascArr := []int{1,7, 9, 10,12,25}
+	if !reflect.DeepEqual(res,ascArr) {
+		t.Fatal("升序失败")
+	}
 }
